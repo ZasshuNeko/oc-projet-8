802b99b6957d95ec9565ff7819b27573446f1fe4
Nicolas Mazaleyrat <nicolas.mazaleyrat@gmail.com>
2020-06-19 09:47:06 +0200

    Pure beurre app

--- OCprojetHuit/settings.py
+++ OCprojetHuit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'x693c28g7ptqmwg)enro2wuxe*6-_wovm)d&i90k9-gx#&_4=l'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
