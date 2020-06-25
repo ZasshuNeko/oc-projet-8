d6b3e65e27713a44f8e5d9234041c9bf13c697ad
Nicolas Mazaleyrat <nicolas.mazaleyrat@gmail.com>
2020-06-25 21:52:41 +0200

    Pure beurre app

--- OCprojetHuit/settings.py
+++ OCprojetHuit/settings.py
@@ -84,11 +84,10 @@ WSGI_APPLICATION = 'OCprojetHuit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
+
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config()
 }
-'''
 '''
 DATABASES = {
     'default': {
