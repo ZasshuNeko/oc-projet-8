6fe345ced8b268f84d5021f514705d67bba919f5
Nicolas Mazaleyrat <nicolas.mazaleyrat@gmail.com>
2020-06-25 17:23:17 +0200

    Pure beurre app

--- OCprojetHuit/settings.py
+++ OCprojetHuit/settings.py
@@ -14,8 +14,6 @@ import os
 import django_heroku
 import dj_database_url
 
-
-django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -144,6 +142,6 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-
+django_heroku.settings(locals())
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
