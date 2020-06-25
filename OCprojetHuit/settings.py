203af8bf0a83616493b6bcf2b2e492f8285f3493
Nicolas Mazaleyrat <nicolas.mazaleyrat@gmail.com>
2020-06-25 16:04:26 +0200

    Pure beurre app

--- OCprojetHuit/settings.py
+++ OCprojetHuit/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'OCprojetHuit.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -93,6 +93,7 @@ DATABASES = {
         'PORT': '', #os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
