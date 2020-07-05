c585cf7b7adafea73cdde55513884ce96a7d7288
Nicolas Mazaleyrat <nicolas.mazaleyrat@gmail.com>
2020-07-05 17:03:21 +0200

    Pure beurre app

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -63,50 +63,50 @@ def maj_bdd(apps, schema_editor):
                 # récupération des catégories
                 categories=item.get("categories")
                 if categories:
-                    #if categories.find(':') == -1:
-                    nw_produit.save()
-                    liste_categories = categories.split(',')
-                    # On vérifie si les catégories existe
-                    for categorie in liste_categories:
-                        categorie = categorie.strip()
-                        try:
-                            object_cat = Categorie.objects.get(
-                                nom__exact=categorie)
-                            object_cat.save()
-                            id_cat = object_cat.id
-                            object_cat.produit.add(nw_produit)
-                        except Categorie.DoesNotExist:
-                            no_accent = "".join((c for c in unicodedata.normalize(
-                            'NFD', categorie) if unicodedata.category(c) != 'Mn'))
-                            nw_categorie = Categorie.objects.create(nom=categorie, nom_iaccents=no_accent)
-                            nw_categorie.save()
-                            id_cat = nw_categorie.id
-                            nw_categorie.produit.add(nw_produit)
-                    liste_store = item.get("stores_tags")
-                    if liste_store:
-                        for stores in item.get("stores_tags"):
-                            Vendeurs.objects.create(
-                                produits=nw_produit, nom=stores)
+                    if categories.find(':') == -1:
+                        nw_produit.save()
+                        liste_categories = categories.split(',')
+                        # On vérifie si les catégories existe
+                        for categorie in liste_categories:
+                            categorie = categorie.strip()
+                            try:
+                                object_cat = Categorie.objects.get(
+                                    nom__exact=categorie)
+                                object_cat.save()
+                                id_cat = object_cat.id
+                                object_cat.produit.add(nw_produit)
+                            except Categorie.DoesNotExist:
+                                no_accent = "".join((c for c in unicodedata.normalize(
+                                'NFD', categorie) if unicodedata.category(c) != 'Mn'))
+                                nw_categorie = Categorie.objects.create(nom=categorie, nom_iaccents=no_accent)
+                                nw_categorie.save()
+                                id_cat = nw_categorie.id
+                                nw_categorie.produit.add(nw_produit)
+                        liste_store = item.get("stores_tags")
+                        if liste_store:
+                            for stores in item.get("stores_tags"):
+                                Vendeurs.objects.create(
+                                    produits=nw_produit, nom=stores)
 
-                    for cle, valeur in liste_nutriment.items():
-                        unit = ""
-                        val_100 = 0
-                        label = ""
-                        if cle.find("_label") == -1:
-                            if cle.find("_unit") != -1:
-                                unit = liste_nutriment.get(cle)
-                                label = cle.split('_')
-                                label = label[0]
-                            elif cle.find("_100g") != -1:
-                                val_100 = liste_nutriment.get(cle)
-                                label = cle.split('_')
-                                label = label[0]
+                        for cle, valeur in liste_nutriment.items():
+                            unit = ""
+                            val_100 = 0
+                            label = ""
+                            if cle.find("_label") == -1:
+                                if cle.find("_unit") != -1:
+                                    unit = liste_nutriment.get(cle)
+                                    label = cle.split('_')
+                                    label = label[0]
+                                elif cle.find("_100g") != -1:
+                                    val_100 = liste_nutriment.get(cle)
+                                    label = cle.split('_')
+                                    label = label[0]
 
-                            if len(unit) != 0 or len(str(val_100)) != 0:
-                                Nutriments.objects.create(
-                                    produits=nw_produit, nom=label, unite=unit, valeur=val_100)
-                                unit = ""
-                                val_100 = 0
+                                if len(unit) != 0 or len(str(val_100)) != 0:
+                                    Nutriments.objects.create(
+                                        produits=nw_produit, nom=label, unite=unit, valeur=val_100)
+                                    unit = ""
+                                    val_100 = 0
 
 
 class Migration(migrations.Migration):
