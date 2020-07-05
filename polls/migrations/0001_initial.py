a7ddab7435bb3674c1b4f1b13bc223f1b1031bde
Nicolas Mazaleyrat <nicolas.mazaleyrat@gmail.com>
2020-07-05 16:02:45 +0200

    Pure beurre app

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -18,7 +18,7 @@ def maj_bdd(apps, schema_editor):
     playload = {
         'action': 'process',
         'sort_by': 'unique_scan_n',
-        'page_size': '2000',
+        'page_size': '5000',
         'json': 'true'
     }
     headers = {}
